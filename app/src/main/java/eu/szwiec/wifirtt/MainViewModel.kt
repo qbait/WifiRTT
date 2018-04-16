52ef0df31c95b9f6ade29ced26efc15e20a714a5
szwiec <jakub.szwiec@navenio.com>
2018-04-16 17:08:15 +0100

    add save to sdcard

--- app/src/main/java/eu/szwiec/wifirtt/MainViewModel.kt
+++ app/src/main/java/eu/szwiec/wifirtt/MainViewModel.kt
@@ -4,6 +4,10 @@ import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.Transformations
 import android.content.Intent
+import android.os.Environment
+import org.apache.commons.io.FileUtils
+import java.io.File
+import java.time.LocalDateTime
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
     val serviceIntent = Intent(getApplication(), RttIntentService::class.java)
@@ -17,5 +21,10 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         } else {
             getApplication<Application>().startService(serviceIntent)
         }
+    }
+
+    fun saveOnSdCard() {
+        val file = File("${Environment.getExternalStorageDirectory().getPath()}/wifi_rtt/${LocalDateTime.now()}")
+        FileUtils.writeStringToFile(file, allResults.value, "UTF-8")
     }
 }
