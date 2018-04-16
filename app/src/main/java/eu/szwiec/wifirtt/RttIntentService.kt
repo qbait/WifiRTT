0b38a8a293c3d66f361891dbc79bb0b69a1dc503
szwiec <jakub.szwiec@navenio.com>
2018-04-16 15:17:50 +0100

    Foreground service

--- app/src/main/java/eu/szwiec/wifirtt/RttIntentService.kt
+++ app/src/main/java/eu/szwiec/wifirtt/RttIntentService.kt
@@ -1,17 +1,23 @@
 package eu.szwiec.wifirtt
 
 import android.app.IntentService
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.arch.lifecycle.MutableLiveData
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.net.wifi.WifiManager
 import android.net.wifi.rtt.RangingRequest
 import android.net.wifi.rtt.RangingResult
 import android.net.wifi.rtt.RangingResultCallback
 import android.net.wifi.rtt.WifiRttManager
-
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.support.v4.app.NotificationCompat
 
 class RttIntentService : IntentService("wifiRttService") {
 
@@ -67,6 +73,7 @@ class RttIntentService : IntentService("wifiRttService") {
     }
 
     override fun onHandleIntent(intent: Intent?) {
+        startForeground()
         wifiManager.startScan()
 
         while (isRunning) {
@@ -75,8 +82,39 @@ class RttIntentService : IntentService("wifiRttService") {
     }
 
     override fun onDestroy() {
+        stopForeground(true)
         isRunning = false
         unregisterReceiver(receiver)
         super.onDestroy()
+    }
+
+    private fun startForeground() {
+        val channelId =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    createNotificationChannel()
+                } else {
+                    ""
+                }
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId )
+        val notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build()
+        startForeground(101, notification)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(): String{
+        val channelId = "my_service"
+        val channelName = "My Background Service"
+        val chan = NotificationChannel(channelId,
+                channelName, NotificationManager.IMPORTANCE_NONE)
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
     }
 }
